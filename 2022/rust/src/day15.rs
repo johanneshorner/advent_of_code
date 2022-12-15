99bb2087f09d16b8d1b78ec39d12c8f7ca6572fc
Johannes Horner <johanneshorner@pm.me>
2022-12-16 00:35:27 +0100

    feat: part1 done

--- 2022/rust/src/day15.rs
+++ 2022/rust/src/day15.rs
@@ -2,9 +2,8 @@ pub fn run(input_lines: &[&str]) {
     let input = parse_input(input_lines);
 
     println!("Part1:\n{}", part1(&input, 2_000_000));
-    println!("Part2:\n{}", part2(&input, 2_000_000));
+    println!("Part2:\n{}", part2(&input, 4_000_000));
 }
-
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Point {
@@ -33,7 +32,10 @@ fn parse_input(input_lines: &[&str]) -> Vec<Sensor> {
                 };
             Sensor {
                 pos: Point { x, y },
-                beacon_pos: Point { x: x_beacon, y: y_beacon },
+                beacon_pos: Point {
+                    x: x_beacon,
+                    y: y_beacon,
+                },
             }
         })
         .collect::<Vec<_>>()
@@ -51,14 +53,79 @@ fn part1(sensors: &[Sensor], row: isize) -> usize {
     for sensor in sensors {
         let max_distance = manhattan_distance(&sensor.pos, &sensor.beacon_pos);
 
-        // non_beacon_positions.extend((sensor.pos.x..sensor.pos.x + max_distance as isize).map(|x| Point { x, y: sensor.pos.y }));
+        for x in sensor.pos.x - max_distance as isize..=sensor.pos.x + max_distance as isize {
+            // we only need to add points that are in the row we are looking for
+            let point = Point { x, y: row };
+            if manhattan_distance(&point, &sensor.pos) <= max_distance {
+                non_beacon_positions.insert(point);
+            }
+        }
     }
 
-    non_beacon_positions.iter().filter(|pos| pos.y == row).count()
+    non_beacon_positions
+        .iter()
+        .filter(|pos| pos.y == row)
+        .count()
+        - 1
 }
 
-fn part2(sensors: &[Sensor], row: isize) -> usize {
-    0
+fn part2(sensors: &[Sensor], max_pos: usize) -> usize {
+    let mut non_beacon_positions = HashSet::new();
+
+    for (i, sensor) in sensors.iter().enumerate() {
+        let max_distance = manhattan_distance(&sensor.pos, &sensor.beacon_pos);
+
+        let left_edge = sensor.pos.x - max_distance as isize;
+        let right_edge = sensor.pos.x + max_distance as isize;
+        let top_edge = sensor.pos.y - max_distance as isize;
+        let bottom_edge = sensor.pos.y + max_distance as isize;
+
+        let x_range = if left_edge <= max_pos as isize && left_edge >= 0 {
+            left_edge as usize..max_pos
+        } else if right_edge <= max_pos as isize && right_edge >= 0 {
+            0..right_edge as usize
+        } else {
+            0..0
+        };
+
+        let y_range = if top_edge <= max_pos as isize && top_edge >= 0 {
+            top_edge as usize..max_pos
+        } else if bottom_edge <= max_pos as isize && bottom_edge >= 0 {
+            0..bottom_edge as usize
+        } else {
+            0..0
+        };
+
+        println!("ranges: {:?} {:?}", x_range, y_range);
+
+        for x in x_range {
+            for y in y_range.clone() {
+                let point = Point {
+                    x: x as isize,
+                    y: y as isize,
+                };
+                if manhattan_distance(&point, &sensor.pos) <= max_distance {
+                    non_beacon_positions.insert(point);
+                }
+            }
+        }
+        println!("{i}");
+    }
+
+    for x in 0..max_pos {
+        for y in 0..max_pos {
+            println!("{x}, {y}");
+            if !non_beacon_positions.contains(&Point {
+                x: x as isize,
+                y: y as isize,
+            }) {
+                println!("found {x}, {y}");
+                return x as usize * 4_000_000 + y as usize;
+            }
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -93,8 +160,8 @@ mod tests {
     #[test]
     fn part2() {
         let input = parse_input(&INPUT_LINES);
-        let result = super::part2(&input, 10);
+        let result = super::part2(&input, 20);
 
-        assert_eq!(45000, result);
+        assert_eq!(56000011, result);
     }
 }
